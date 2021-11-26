d50fdffb305a392c532f969f1c6e7ad26325027c
tetianasirenko <56349935+tetianasirenko@users.noreply.github.com>
2021-11-26 15:52:43 +0200

    Add some utils and constants. Add map for storing coordinates and entities. Add base algorithm for building house

--- my_strategy.py
+++ my_strategy.py
@@ -1,50 +1,62 @@
-from decider.enemies_detector import EnemiesDetector
 from model import *
-
-from decider.units_storage import UnitsStorage
+from resource_processor import ResourceProcessor
+from map_processor import MapProcessor
+import constants
 
 
 class MyStrategy:
     def get_action(self, player_view, debug_interface):
         result = Action({})
         my_id = player_view.my_id
+        house_coord = None
+        house_builder = constants.HOUSE_BUILDERS
 
-        units_storage = UnitsStorage(my_id)
-        units_storage.update_storage(player_view.entities)
+        resource_processor = ResourceProcessor(my_id, player_view.entities)
+        map_processor = MapProcessor(player_view)
 
-        enemies_detector = EnemiesDetector()
-        enemies_detector.check_collisions(units_storage.get_allies(), units_storage.get_enemies())
-
-        print("Spotted %d enemies" % len(enemies_detector.get_collisions()))
+        current_resource = resource_processor.getMyPlayerResources(player_view)
+        print (current_resource)
 
         for entity in player_view.entities:
+
             if entity.player_id != my_id:
                 continue
             properties = player_view.entity_properties[entity.entity_type]
+            print(properties)
 
             move_action = None
             build_action = None
-            if properties.can_move:
-                move_action = MoveAction(
-                    Vec2Int(player_view.map_size - 1,
-                            player_view.map_size - 1),
-                    True,
-                    True)
-            elif properties.build is not None:
-                entity_type = properties.build.options[0]
-                current_units = 0
-                for other_entity in player_view.entities:
-                    if my_id == other_entity.player_id and other_entity.entity_type == entity_type:
-                        current_units += 1
-                if (current_units + 1) * player_view.entity_properties[entity_type].population_use <= properties.population_provide:
+
+            #if properties.build == EntityType.HOUSE:
+            #    continue
+
+            if house_builder > 0 and resource_processor.canBuildHouse() and entity.entity_type == EntityType.BUILDER_UNIT:
+                if house_coord is None:
+                    house_coord = resource_processor.findHouseCoordinates(player_view.map_size, map_processor.get_map())
+                print("House=", house_coord)
+
+                if house_coord:
                     build_action = BuildAction(
-                        entity_type,
+                        EntityType.HOUSE,
+                        Vec2Int(house_coord[0], house_coord[1]))
+                    house_builder = house_builder-1
+
+            elif current_resource > constants.POPULATION_LIMIT:
+                build_action = BuildAction(
+                        EntityType.BUILDER_UNIT,
                         Vec2Int(entity.position.x + properties.size, entity.position.y + properties.size - 1))
+                if properties.can_move:
+                    move_action = MoveAction(
+                        Vec2Int(player_view.map_size - 1,
+                                player_view.map_size - 1),
+                        True,
+                        True)
+
             result.entity_actions[entity.id] = EntityAction(
                 move_action,
                 build_action,
                 AttackAction(None, AutoAttack(properties.sight_range, [
-                             EntityType.RESOURCE] if entity.entity_type == EntityType.BUILDER_UNIT else [])),
+                    EntityType.RESOURCE] if entity.entity_type == EntityType.BUILDER_UNIT else [])),
                 None
             )
         return result
